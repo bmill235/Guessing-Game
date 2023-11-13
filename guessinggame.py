26b669e792c58056e95fb65337406aa5d2571632
Benjamin Miller (Class of 2025) <251190bm@salesianum.org>
2023-11-13 10:50:00 -0500

    Second commit

--- guessinggame.py
+++ guessinggame.py
@@ -2,9 +2,9 @@ def main():
     import random
     number = random.randint(1,100)
     number_reveal = ("The number was: " + str(number))
-    def play_game():
+    def play_game(guess):
         def give_feedback(number, guess, number_reveal):
-            if guess > 100 or guess < 1:
+            if guess >= 100 or guess <= 1:
                 print("Number must be between 1 and 100")
             elif number == guess:
                 print("You Win!")
@@ -12,13 +12,14 @@ def main():
                 print("Your guess was too high. Try again.")
             elif number >= guess:
                 print("Your guess was too low. Try again.")
+        give_feedback(number, guess, number_reveal)
     def gameLoop():
         lives = 3
-        for game in lives:
-            play_game(input("Guess a number from 1 to 100."))
-            while lives <= 0:
-                return "You lose!" + number_reveal
+        for game in range(lives):
+            play_game(int(input("Guess a number from 1 to 100: ")))
             lives -= 1
+            while lives == 0:
+                print("You lose! " + number_reveal)
     print(gameLoop())
     
 if __name__ == "__main__":
