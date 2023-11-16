069ce0e22a808db1f6875057a3a6663b18e2fa55
Benjamin Miller (Class of 2025) <251190bm@salesianum.org>
2023-11-16 11:56:53 -0500

    Final commit.

--- guessinggame.py
+++ guessinggame.py
@@ -2,32 +2,17 @@ import random
 def main():
     print("Welcome to the Simple Number Guessing Game. The rules are, as the name suggests, simple. Guess a number from 1 to 100. You have 3 lives to do so, and your attempts will be tracked. Good Luck!")
     play_game()
-<<<<<<< HEAD
 
-=======
-   
->>>>>>> 6b11a85a83b9e919f6bc1be5c31b1268469187bf
 def play_game():
     print(gameLoop())
     print(restart())
 
-<<<<<<< HEAD
 def generate_random_number():
     return random.randint(1,100)
 
 def player_guess(guess):
     print("You guessed " + str(guess))
     
-=======
-
-def generate_random_number():
-    return random.randint(1,100)
-
-
-def player_guess(guess):
-    print("You guessed " + str(guess))
-   
->>>>>>> 6b11a85a83b9e919f6bc1be5c31b1268469187bf
 def give_feedback(number, guess, lives):
     if number == guess:
         return "You Win!"
@@ -40,7 +25,6 @@ def give_feedback(number, guess, lives):
             return "Your guess was too low. Try again."
     else:
         return "Better luck next time."
-<<<<<<< HEAD
     
 def gameLoop():    
     number = generate_random_number()
@@ -50,24 +34,6 @@ def gameLoop():
     for game in range(lives):
         number_guess = int(input("Guess a number from 1 to 100: "))
         print(player_guess(number_guess))
-=======
-   
-def restart():
-    start_again = input("Would you like to play again? Type 'Yes' or 'No': ")
-    if start_again == "Yes" or start_again == "yes":
-        play_game()
-    else:
-        return("Hope you had fun. Thank you for playing. See you next time.")
-       
-def gameLoop():    
-    number = generate_random_number()
-    number_reveal = ("The number was: " + str(number))
-    lives = 3
-    attempts = 0
-    for game in range(lives):
-        number_guess = int(input("Guess a number from 1 to 100: "))
-        player_guess(number_guess)
->>>>>>> 6b11a85a83b9e919f6bc1be5c31b1268469187bf
         print(give_feedback(number,number_guess, lives))
         lives -= 1
         attempts += 1
@@ -82,17 +48,12 @@ def gameLoop():
             return("You Lose! " + number_reveal)
             restart()
 
-<<<<<<< HEAD
 def restart():
     start_again = input("Would you like to play again? Type 'Yes' or 'No': ")
     if start_again == "Yes" or start_again == "yes":
         play_game()
     else:
         return("Hope you had fun. Thank you for playing. See you next time.")
-=======
->>>>>>> 6b11a85a83b9e919f6bc1be5c31b1268469187bf
 
 if __name__ == "__main__":
     main()
-
-
